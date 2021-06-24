edeebe0754be5b5375cf4cd615f1c4388b0666e6
Adir <42638312+Adir-SL@users.noreply.github.com>
2021-06-24 11:36:29 +0300

    Update twpurge.js

--- twpurge.js
+++ twpurge.js
@@ -33,6 +33,10 @@ function findTailwind(){
     var x = window.tailwindFilter;
     var i;
     for (i = 0; i < x.length; i++) {
+        if(x[i].indexOf(":") > -1){
+            x[i] = x[i].slice(x[i].indexOf(":")+1);
+            console.log(x[i]);
+        }
         matchTailwind(x[i]);
     }
     console.log(window.saveRules);
